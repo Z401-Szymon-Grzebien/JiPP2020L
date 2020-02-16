19c1a4af1bafbf5d6054d7a31a222adf60d4d00d
g_rodewald <g_rodewald@WWSI-0407>
2020-02-16 11:04:32 +0100

    Pierwszy commit.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class cTemp
+{
+    public double celsjusz=0.0, farenheit=0.0, wynik=0.0;
+    public double Przelicz()
+    {
+       
+
+        return wynik;
+    }
+
+}
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Podaj stopnie w Celsjuszach: ");
+            double wpiszstopnieC = Console.ReadLine();
+
+
+
+        }
+    }
+}
